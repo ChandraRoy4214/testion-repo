e3a919c65547d8ac2d691259355e4a798cadd690
Chandra Roy <chandraroycoder@gmail.com>
2021-01-29 14:14:43 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+function first(){
+    console.log("first");
+}
+function second(){
+    console.log("second");
+}
+function third(){
+    console.log("third");
+}
+function fourth(){
+    console.log("four");
+    first()
+    second()
+    third()
+}
+fourth()
